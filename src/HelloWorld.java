33fa46f0483b2e2578c29df382928b737bc3f30c
JingtQ <506655892@qq.com>
2020-07-14 18:32:49 +0800

    0.0

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String []args){
         System.out.println("Hello world," +
-                " and it will pull onto the github.OTO");
+                " and it will pull onto the github.0.0");
     }
 }
